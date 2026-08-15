6a980dd542c548692d19540a42a87f837dad01ba
catalin-enache <catalinenache78@gmail.com>
2026-08-15 22:15:34 +0300

    init

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,8 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import reactX from 'eslint-plugin-react-x';
+import reactDom from 'eslint-plugin-react-dom';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
@@ -15,7 +17,9 @@ export default defineConfig([
       reactRefresh.configs.vite,
       tseslint.configs.recommendedTypeChecked,
       tseslint.configs.strictTypeChecked,
-      tseslint.configs.stylisticTypeChecked
+      tseslint.configs.stylisticTypeChecked,
+      reactX.configs['recommended-typescript'],
+      reactDom.configs.recommended
     ],
     languageOptions: {
       globals: globals.browser,
