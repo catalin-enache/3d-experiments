79a721155a52bd71bfe8fbbe2383cc75b558cd60
catalin-enache <catalinenache78@gmail.com>
2026-09-14 12:38:02 +0300

    Turtle done

--- python/PdB/Turtle.py
+++ python/PdB/Turtle.py
@@ -1,7 +1,10 @@
+import math
+
 import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
+import numpy as np
 from lib.utils import *
 
 pygame.init()
@@ -16,6 +19,47 @@ ortho_bottom = 400
 screen = pygame.display.set_mode((screen_width, screen_height), DOUBLEBUF | OPENGL)
 pygame.display.set_caption('Turtle Graphics')
 
+position = (0, 0)
+direction = np.array([0, 1, 0])
+
+def line_to(x, y):
+    global position
+    glBegin(GL_LINE_STRIP)
+    glVertex2f(position[0], position[1])
+    glVertex2f(x, y)
+    # glVertex2f(current_position[0] + x, current_position[1] + y)
+    glEnd()
+    position = (x, y)
+
+
+def move_to(x, y):
+    global position
+    position = (x, y)
+
+
+def reset_turtle():
+    global position
+    global direction
+    position = (0, 0)
+    direction = np.array([0, 1, 0])
+
+
+def draw_turtle():
+    for i in range(20):
+        forward(200)
+        rotate(170)
+
+
+def forward(draw_length):
+    new_x = position[0] + direction[0] * draw_length
+    new_y = position[1] + direction[1] * draw_length
+    line_to(new_x, new_y)
+
+
+def rotate(angle):
+    global direction
+    direction = z_rotation(direction, math.radians(angle))
+
 
 def init_ortho():
     glMatrixMode(GL_PROJECTION)
@@ -24,8 +68,9 @@ def init_ortho():
 
 
 init_ortho()
+glLineWidth(5)
+
 done = False
-glLineWidth(1)
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -37,6 +82,10 @@ while not done:
     glBegin(GL_POINTS)
     glVertex2f(0, 0)
     glEnd()
+
+    reset_turtle()
+    draw_turtle()
+
     pygame.display.flip()
 pygame.quit()
 
--- python/PdB/lib/utils.py
+++ python/PdB/lib/utils.py
@@ -15,15 +15,27 @@ def map_value(current_min: float,
 
 
 def x_rotation(vector, theta):
-    new_vector = np.array([[1, 0, 0], [0, np.cos(theta), -np.sin(theta)], [0, np.sin(theta), np.cos(theta)]])
-    return np.dot(new_vector, vector)
+    rotation_matrix = np.array([
+        [1, 0, 0],
+        [0, np.cos(theta), -np.sin(theta)],
+        [0, np.sin(theta), np.cos(theta)]])
+    # return np.dot(rotation_matrix, vector)
+    return rotation_matrix @ vector
 
 
 def y_rotation(vector, theta):
-    new_vector = np.array([[np.cos(theta), 0, np.sin(theta)], [0, 1, 0], [-np.sin(theta), 0, np.cos(theta)]])
-    return np.dot(new_vector, vector)
+    rotation_matrix = np.array([
+        [np.cos(theta), 0, np.sin(theta)],
+        [0, 1, 0],
+        [-np.sin(theta), 0, np.cos(theta)]])
+    # return np.dot(rotation_matrix, vector)
+    return rotation_matrix @ vector
 
 
 def z_rotation(vector, theta):
-    new_vector = np.array([[np.cos(theta), -np.sin(theta), 0], [np.sin(theta), np.cos(theta), 0], [0, 0, 1]])
-    return np.dot(new_vector, vector)
+    rotation_matrix = np.array([
+        [np.cos(theta), -np.sin(theta), 0],
+        [np.sin(theta), np.cos(theta), 0],
+        [0, 0, 1]])
+    # return np.dot(new_vector, vector)
+    return rotation_matrix @ vector
