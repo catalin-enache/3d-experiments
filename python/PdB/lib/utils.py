dfc226e54e4c2630c96c19b53691350eb726eaef
catalin-enache <catalinenache78@gmail.com>
2026-09-14 09:12:42 +0300

    Turtle init

--- python/PdB/lib/utils.py
+++ python/PdB/lib/utils.py
@@ -1,3 +1,6 @@
+import math
+import numpy as np
+
 def map_value(current_min: float,
               current_max: float,
               new_min: float,
@@ -9,3 +12,18 @@ def map_value(current_min: float,
             / (current_max - current_min)
             * (new_max - new_min)
     )
+
+
+def x_rotation(vector, theta):
+    new_vector = np.array([[1, 0, 0], [0, np.cos(theta), -np.sin(theta)], [0, np.sin(theta), np.cos(theta)]])
+    return np.dot(new_vector, vector)
+
+
+def y_rotation(vector, theta):
+    new_vector = np.array([[np.cos(theta), 0, np.sin(theta)], [0, 1, 0], [-np.sin(theta), 0, np.cos(theta)]])
+    return np.dot(new_vector, vector)
+
+
+def z_rotation(vector, theta):
+    new_vector = np.array([[np.cos(theta), -np.sin(theta), 0], [np.sin(theta), np.cos(theta), 0], [0, 0, 1]])
+    return np.dot(new_vector, vector)
